222fc58272174aa8545262d0dafc4e2f6fa9ce3d
Dominique Dumont <ddumont@cpan.org>
2008-01-28 12:43:08 +0000

    Import of DDUMONT/Config-Model-Xorg-0.507 from CPAN.
    gitpan-cpan-distribution: Config-Model-Xorg
    gitpan-cpan-version:      0.507
    gitpan-cpan-path:         DDUMONT/Config-Model-Xorg-0.507.tar.gz
    gitpan-cpan-author:       DDUMONT
    gitpan-cpan-maturity:     released

--- lib/Config/Model/Xorg/Read.pm
+++ lib/Config/Model/Xorg/Read.pm
@@ -1,7 +1,7 @@
 # $Author: ddumont $
-# $Date: 2007/06/07 16:46:47 $
+# $Date: 2008/01/23 11:12:16 $
 # $Name:  $
-# $Revision: 1.3 $
+# $Revision: 1.5 $
 
 #    Copyright (c) 2005,2006 Dominique Dumont.
 #
@@ -29,6 +29,10 @@ use Carp ;
 use IO::File ;
 use Log::Log4perl;
 use Data::Dumper ;
+
+use vars qw($VERSION) ;
+
+$VERSION = sprintf "%d.%03d", q$Revision: 1.5 $ =~ /(\d+)\.(\d+)/;
 
 my $logger = Log::Log4perl::get_logger(__PACKAGE__);
 
@@ -213,7 +217,7 @@ sub parse_option {
 	# dont' work for ServerFlags
 	$logger->debug( "obj ",$obj->name, " ($line) load option '$opt' ");
 	my $opt_obj = $obj->fetch_element("Option")->fetch_element($opt) ;
-	$opt_obj->store ( @args  ? $args[0] : 1 ) ;
+	$opt_obj->store ( @args  ? $args[0] : 1 ) if defined $opt_obj ;
     }
 }
 
@@ -395,8 +399,12 @@ sub parse_section {
     # first get the identifier and create the object. 
     if ($has_id) {
 	my $id_rr =  delete $section_data->{Identifier}
-	          || delete $section_data->{Depth}
-	          || die "can't find identifier for ",$obj->name ;
+	          || delete $section_data->{Depth} ;
+	if (not defined $id_rr) {
+	    $logger->debug( "parse_section can't find identifier for ",$obj->name );
+	    return ;
+	}
+
 
 	my ($line,$id) = @{$id_rr->[0]}  ;
 	$logger->debug( "parse_section $line: found id '$id' for '",
--- lib/Config/Model/Xorg/Write.pm
+++ lib/Config/Model/Xorg/Write.pm
@@ -1,9 +1,9 @@
 # $Author: ddumont $
-# $Date: 2007/06/07 16:47:00 $
+# $Date: 2008/01/28 12:10:52 $
 # $Name:  $
-# $Revision: 1.4 $
+# $Revision: 1.6 $
 
-#    Copyright (c) 2005,2006 Dominique Dumont.
+#    Copyright (c) 2005-2008 Dominique Dumont.
 #
 #    This file is part of Config-Xorg.
 #
@@ -29,6 +29,10 @@ use Carp ;
 use IO::File ;
 use Config::Model::ObjTreeScanner ;
 use Log::Log4perl ;
+
+use vars qw($VERSION) ;
+
+$VERSION = sprintf "%d.%03d", q$Revision: 1.6 $ =~ /(\d+)\.(\d+)/;
 
 my $logger = Log::Log4perl::get_logger(__PACKAGE__);
 
@@ -78,7 +82,7 @@ sub wr_std_options {
 sub wr_kbd_model_options {
     my ($scanner, $data_r ,$node,$element_name,$index, $leaf_object,$v) = @_ ;
     if ( defined $v && $v ) {
-	push @$data_r, qq(\tOption\tXkbOptions\t"$element_name:$v");
+	push @$data_r, qq(\tOption\t"XkbOptions"\t"$element_name:$v");
     }
 } ;
 
